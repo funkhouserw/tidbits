a9294bf27907022eefc0e4435423035773e882b8
funkhouserw <wilfunkhouser@gmail.com>
2017-06-15 16:40:15 -0700

    add week 3 helper

--- helpers.R
+++ helpers.R
@@ -70,3 +70,13 @@ loadOrInstall <- function(thePack) {
 }
 
 
+
+# regsubset predictor from week 3 ds740 
+predict.regsubsets <- function(object,newdata,id,...){
+  form = as.formula(object$call[[2]])
+  mat = model.matrix(form,newdata)
+  coefi = coef(object,id=id)
+  xvars = names(coefi)
+  mat[,xvars]%*% coefi
+  
+} 
